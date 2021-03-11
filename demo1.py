dac43d26bc7df0059b96dba96c73a3f2147ae254
System Administrator <root@songdeMacBook-Pro.local>
2021-03-11 22:15:57 +0800

    first_commit

---
+++ demo1.py
@@ -0,0 +1,15 @@
+
+sum = 0
+for i in range(1,101) :
+	sum += i
+print(sum)
+
+str = ["aa","bb","cc","dd","ee"]
+
+#for i in range(len(str)) :
+#	print(str[i])
+
+i = 0
+while i<len(str) :
+	print(str[i])
+	i += 1	 
